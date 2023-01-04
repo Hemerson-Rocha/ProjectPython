386ac70a236c773d987f17390c59669e961387b3
Hemerson-Rocha <hemerson.me@gmail.com>
2023-01-04 17:20:51 -0300

    aprendendo mais sobre staticfiles, erros 404 e 500, redirecionamento apos logout

---
+++ core/static/js/script.js
@@ -0,0 +1,3 @@
+function teste(){
+    alert("tu clicou vei...")
+}
--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Produto
+from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
 
@@ -27,7 +29,8 @@ def contato(request):
 
 def ver_produto(request, pk):
 
-    produto = Produto.objects.get(pk=pk)
+    # produto = Produto.objects.get(pk=pk)
+    produto = get_object_or_404(Produto, pk=pk)
 
     print(produto)
 
@@ -36,3 +39,11 @@ def ver_produto(request, pk):
     }
 
     return render(request, 'produto.html', contexto)
+
+def error404(request, exception):
+    template = loader.get_template('404.html')# colocar um template em uma var
+    return HttpResponse(content=template.render(), content_type='text/html; charset=utf8', status=404)
+
+def error500(request):
+    template = loader.get_template('500.html')# colocar um template em uma var
+    return HttpResponse(content=template.render(), content_type='text/html; charset=utf8', status=500)
